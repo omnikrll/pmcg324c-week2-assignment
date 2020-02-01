66d1de7ab29601d2481c29ff765f9a5691fda353
AnneBenissan <32121441+AnneBenissan@users.noreply.github.com>
2020-01-31 19:44:50 -0500

    updated JS to create click and function

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ var text = document.getElementById(text)
 clickMe.addEventListener("click", updateDemo);
 
 function updateDemo(){
-  text.innerHTML = "correctAnswers"
+  text.innerHTML = "Correct Answers";
 }
