2cdbf59afeeae2dc7b89da5c936fd61a120215c3
Karl Hohn <kfhohn@gmail.com>
2020-02-11 18:10:47 -0500

    karl changes

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-var clickMe = document.getElementById("clickMe")
-var text = document.getElementById("text")
+var clickMe = document.getElementById("clickMe");
+var text = document.getElementById("text");
+
+// document.querySelector('#clickMe');
+// document.querySelector('#text');
+// document.querySelectorAll('.answers p');
+// document.getElementsByClassName('.answers')[0].getElementsByTagName('p');
+
+var div = document.querySelectorAll('.demo-module')[0];
 
 clickMe.addEventListener("click", updateDemo);
 
 function updateDemo(){
   text.innerHTML = "Your answers are correct!";
+  console.log(div.classList);
+  div.classList.add('different');
 }
